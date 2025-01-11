1a6406d5a1107960c5ab4eba24b959b88b763089
Siberg1 <sibergshow684@gmail.com>
2025-01-10 19:56:37 -0500

    Finished. Now working

--- Program.cs
+++ Program.cs
@@ -27,8 +27,62 @@ class Calculator
 }
 class Program
 {
+    static double ReadAndValidateNumber(string prompt)
+    {
+        double num;
+        Console.Write(prompt);
+        while (!double.TryParse(Console.ReadLine(), out num))
+        {
+            Console.Write("This is not valid input. Please enter a numeric value: ");
+        }
+        return num;
+    }
+
     static void Main(string[] args)
     {
-        
-    }
-} 
+        bool endApp = false;
+        while(!endApp)
+        {
+            
+            double result = 0;
+
+
+            double cleanNum1 = ReadAndValidateNumber("Type a first number, and then press Enter: ");
+            double cleanNum2 = ReadAndValidateNumber("Type a second number, and then press Enter: ");
+
+            Console.WriteLine("Choose an operator from the following list: \ta - Add \ts - Subtract \tm - Multiply \td - Divide \nYour option?");
+            string? op = Console.ReadLine();
+
+            if (op == null || !Regex.IsMatch(op, "^[asmd]$"))
+            {
+                Console.WriteLine("Error: Unrecognized input.");
+                continue;  // Пропуск выполнения операции, если ввод некорректен
+            }
+            else
+            { 
+                try
+                {
+                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    if (double.IsNaN(result))
+                    {
+                        Console.WriteLine("This operation will result in a mathematical error.\n");
+                    }
+                    else Console.WriteLine("Your result: {0:0.##}\n", result);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oh no! An exception occurred trying to do the math.\n - Details: " + e.Message);
+                }
+            }
+
+            //Цикл, который просит пользователя продолжить или завершить программу   
+            Console.Write("Would you like to continue? (Y = yes, N = No): ");
+            string? input = Console.ReadLine();
+            if (input == null || input.ToUpper() != "Y")
+            {
+                Console.WriteLine("Bye!");
+                break;
+            }              
+        }
+    }           
+}
